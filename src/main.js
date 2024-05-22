405d3d17d0a6a30b129404d116d8eff155677ff9
Ihorsoft <0639492002s@gmail.com>
2024-05-22 11:52:04 +0300

    add flatpickr

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // import './css/styles.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 // import SimpleLightbox from "simplelightbox";
 // import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 const lightbox = new SimpleLightbox('.some-element a', { /* options */ });
