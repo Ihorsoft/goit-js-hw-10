a6a63ac64c10d7cd6b5705745a3949282db0d3a9
Ihorsoft <0639492002s@gmail.com>
2024-05-22 11:30:15 +0300

    2024-05-22

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import './css/styles.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// import SimpleLightbox from "simplelightbox";
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+const lightbox = new SimpleLightbox('.some-element a', { /* options */ });
